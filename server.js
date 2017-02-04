af59fe6a4f9c835ed27ff2164f7ab9a58c85b2a2
suhailparad <pk.suhail47@gmail.com>
2017-02-04 16:28:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var article={
+var articles={
     'article-one':{
         title:"Article One | Muhammed Suhail",
         heading:"Article One",
