128bc256c4da7158a6dde34eb442c48a75b972d3
suhailparad <pk.suhail47@gmail.com>
2017-02-24 20:50:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.get('/:articleName',function(req,res){
     res.send(getArticles(articles[articleName]));
 });
 
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
