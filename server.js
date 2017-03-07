c3f8c6aad0384f241f8e56ee8b256b2b05d329cd
suhailparad <pk.suhail47@gmail.com>
2017-03-07 22:09:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,11 +69,6 @@ function getArticles(data){
     return htmlTemplate;
 }
 
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
-    res.send(getArticles(articles[articleName]));
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
